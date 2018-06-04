4e38c17e6ab66e6220821e96121a205e1e259ad7
Meira <meirabloom@gmail.com>
2018-06-04 16:49:49 +0300

    b

--- src/fileprocessing/order/TypeOrder.java
+++ src/fileprocessing/order/TypeOrder.java
@@ -5,13 +5,23 @@ import java.util.Comparator;
 
 public class TypeOrder extends Order {
 
+    private static final String SPLIT_BY = "\\.";
+    private static final String NO_TYPE = " ";
+
     public TypeOrder(){
         orderBy = new Comparator<File>() {
             @Override
             public int compare(File o1, File o2) {
-                String o1Path = o1.getAbsolutePath();
-                String o2Path = o2.getAbsolutePath();
-                return o1Path.compareTo(o2Path);
+                String[] o1Name = o1.getName().split(SPLIT_BY);
+                String[] o2Name = o2.getName().split(SPLIT_BY);
+                String o1Type = o1Name.length <= 1 ? NO_TYPE : o1Name[o1Name.length - 1];
+                String o2Type = o2Name.length <= 1 ? NO_TYPE : o2Name[o2Name.length - 1];
+                if (!o1Type.equals(o2Type)){
+                    return o1Type.compareTo(o2Type);
+                }
+                else {
+                    return o1.getAbsolutePath().compareTo(o2.getAbsolutePath());
+                }
             }
         };
     }
