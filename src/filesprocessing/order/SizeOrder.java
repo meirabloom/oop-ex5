ebc93a4d5c8033aea3128949ebf73f80402952c7
Meira <meirabloom@gmail.com>
2018-06-05 19:59:37 +0300

    b

--- src/filesprocessing/order/SizeOrder.java
+++ src/filesprocessing/order/SizeOrder.java
@@ -12,10 +12,15 @@ public class SizeOrder extends Order {
         orderBy = new Comparator<File>() {
             @Override
             public int compare(File o1, File o2) {
-                double o1Size = o1.length() / CONVERT_TO_KB ;
+                double o1Size = o1.length() / CONVERT_TO_KB;
                 double o2Size = o2.length() / CONVERT_TO_KB;
-                return o1Size < o2Size ? -1 : o1 == o2 ? 0 : 1;
+                if (!(o1Size == o2Size)) {
+                    return o1Size < o2Size ? -1 : 1;
                 }
+                else {
+                    return o1.getAbsolutePath().compareTo(o2.getAbsolutePath());
+                }
+            }
         };
     }
 }
