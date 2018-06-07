b7be3bbf7da92c22566d1bc18fdf49aa5ef10c87
Meira <meirabloom@gmail.com>
2018-06-07 21:26:05 +0300

    b

--- src/filesprocessing/OrderFactory.java
+++ src/filesprocessing/OrderFactory.java
@@ -1,15 +1,24 @@
 package filesprocessing;
 import filesprocessing.order.*;
 
-
+/**
+ * returns the required order object according to the command
+ */
 public class OrderFactory {
 
     private static final String HASHTAG = "#";
     private static final String ABS = "abs";
     private static final String SIZE = "size";
     private static final String TYPE = "type";
+    private static final String EMPTY_STRING = "";
     private static final String REVERSE = "REVERSE";
 
+    /**
+     *
+     * @param orderMethod - the string representing the required order
+     * @param lineNum - the number of the line of the string
+     * @return - the order object according to the requirement
+     */
     public Order createOrder(String orderMethod, int lineNum){
         String[] args = orderMethod.split(HASHTAG);
         boolean toReverse = args[args.length - 1].equals(REVERSE);
@@ -20,7 +29,7 @@ public class OrderFactory {
                 return new SizeOrder(toReverse, lineNum);
             case TYPE:
                 return new TypeOrder(toReverse,lineNum);
-            case "":
+            case EMPTY_STRING:
                 return new AbsOrder(false,lineNum);
             default:
                 return new AbsOrder(lineNum);
