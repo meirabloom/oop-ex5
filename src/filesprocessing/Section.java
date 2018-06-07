4221ceef6f94955a8a70a08d27f3153ac0754324
Meira <meirabloom@gmail.com>
2018-06-07 21:03:06 +0300

    b

--- src/filesprocessing/Section.java
+++ src/filesprocessing/Section.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class represents a sub-Section, that contains it's filter and order, and when given sourceDir file
- * filter it and orders the filtered files
+ * This class represents a Section object that was created by the section factory, and contains it's filter
+ * and order, and when given sourceDir file filter it and orders the filtered files
  */
 public class Section {
 
@@ -17,11 +17,21 @@ public class Section {
     private Order myOrder;
     private List<File> filteredFile;
 
+    /**
+     * the constructor
+     * @param filter - the type of filter required
+     * @param order - the type of order required
+     */
     Section(Filter filter, Order order){
         this.myFilter = filter;
         this.myOrder = order;
     }
 
+    /**
+     * adds the files that pass the filter requirements into an array, and sorts them according to the
+     * specific command
+     * @param sourceDir the files
+     */
     void getFile(File sourceDir){
         filteredFile = new ArrayList<>();
         File[] sourceDirFiles = sourceDir.listFiles(File::isFile);
@@ -33,12 +43,18 @@ public class Section {
        myOrder.orderFiles(filteredFile);
     }
 
+    /**
+     * prints the names of the files that have passed the filter, in the sorted order
+     */
     void printFile(){
         for (File aFilteredFile : filteredFile) {
             System.out.println(aFilteredFile.getName());
         }
     }
 
+    /**
+     * prints the warnings from each subsection
+     */
     void printWarning(){
         myFilter.printWarning();
         myOrder.printWarning();
